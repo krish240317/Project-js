6c7818918a5b53167de97b8e53a16ef3f982420c
krish240317 <krishvaghela1710@gmail.com>
2024-05-10 04:28:29 +0530

    Completed Tic Tac Toe

--- Tic Tac Toe/main.js
+++ Tic Tac Toe/main.js
@@ -1,8 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let reset = document.querySelector(".reset");
-let newgame=document.querySelector("#newgame");
-let winnerdiv=document.querySelector(".winner");
-let msg=document.querySelector("#msg");
+let newgame = document.querySelector("#newgame");
+let winnerdiv = document.querySelector(".winner");
+let msg = document.querySelector("#msg");
 let turn0 = true;
 
 const winpattern = [
@@ -16,12 +16,11 @@ const winpattern = [
   [2, 4, 6],
 ];
 
+const resetgame = () => {
+  turn0 = true;
+  enable();
 
-const resetgame=()=>{
-    turn0 = true;
-    enable();
-   
-    winnerdiv.classList.add("hide");
+  winnerdiv.classList.add("hide");
 };
 //creat event listner for each button click
 
@@ -45,52 +44,43 @@ boxes.forEach((box) => {
   });
 });
 
-//enalble function to reset the game 
-const enable=()=>{
-    for(let box of boxes)
-        {
-            box.disabled=false;
-            box.innerText="";
-        }
+//enalble function to reset the game
+const enable = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
 };
 
-const disablebuttons=()=>{
-    for(let box of boxes)
-        {
-            box.disabled=true;
-        }
-}
-const Showwinner=(winner)=>{
-    msg.innerText=`Congratulation Winner is ${winner}`;
-    winnerdiv.classList.remove("hide");
-    //disable function so as we get winner one time cant do anything other 
-    disablebuttons();
+const disablebuttons = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+const Showwinner = (winner) => {
+  msg.innerText = `Congratulation Winner is ${winner}`;
+  winnerdiv.classList.remove("hide");
+  //disable function so as we get winner one time cant do anything other
+  disablebuttons();
+};
 
-}
-
-
-const checkwinner= ()=>{
-    for(let pattern of winpattern)
-        {
-            //pattern 0 gives value of win which are store in array 
-            //boxes of winpattern (1 to 8 ) and then its inner text 
-            let posi1_value=boxes[pattern[0]].innerText;
-            let posi2_value =boxes[pattern[1]].innerText;
-            let posi3_value=boxes[pattern[2]].innerText;
-            //check all same or  not 
-            if(posi1_value!="" && posi2_value!="" && posi3_value!="")
-                {
-                    if(posi1_value==posi2_value && posi2_value==posi3_value)
-                        {
-                            console.log("Winner",posi1_value);
-                            Showwinner(posi1_value);
-
-                        }
-                }
-
-        }
+const checkwinner = () => {
+  for (let pattern of winpattern) {
+    //pattern 0 gives value of win which are store in array
+    //boxes of winpattern (1 to 8 ) and then its inner text
+    let posi1_value = boxes[pattern[0]].innerText;
+    let posi2_value = boxes[pattern[1]].innerText;
+    let posi3_value = boxes[pattern[2]].innerText;
+    //check all same or  not
+    if (posi1_value != "" && posi2_value != "" && posi3_value != "") {
+      if (posi1_value == posi2_value && posi2_value == posi3_value) {
+        console.log("Winner", posi1_value);
+        Showwinner(posi1_value);
+      }
+    }
+  }
 };
 //event listners to reset the  game or new game
 
-reset.addEventListener("click",resetgame);
-newgame.addEventListener("click",resetgame);
+reset.addEventListener("click", resetgame);
+newgame.addEventListener("click", resetgame);
